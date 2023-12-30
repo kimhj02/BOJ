eb516fb4a33d7916bc7c6bde5e0001d846b6f1f6
kimhj02 <140468353+kimhj02@users.noreply.github.com>
2023-12-30 17:10:24 +0900

    BOJ16395.c

---
+++ BOJ16395.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int d[31][31];
+int main() 
+{
+    for (int i = 0; i < 31; i++) {
+        d[i][0] = 1;
+    }
+    for (int i = 1; i < 31; i++) 
+    {
+        for (int j = 1; j <= i; j++) 
+        {
+            d[i][j] = d[i - 1][j - 1] + d[i - 1][j];
+        }
+    }
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", d[a-1][b-1]);
+    return 0;
+}
