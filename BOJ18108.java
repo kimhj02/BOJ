5731cc8d69cbfcf4fdbd511bd5239dc8fe534a4f
kimhj02 <140468353+kimhj02@users.noreply.github.com>
2024-01-07 19:50:08 +0900

    Add files via upload

---
+++ BOJ18108.java
@@ -0,0 +1,12 @@
+package BOJ_JAVA;
+
+import java.util.Scanner;
+
+public class BOJ18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+        System.out.println(num - 543);
+    }
+}
