63668641b1e85858236d967d236561fe0aed0e99
kimhj02 <140468353+kimhj02@users.noreply.github.com>
2024-01-07 19:29:18 +0900

    BOJ1000 JAVA

---
+++ BOJ1000.java
@@ -0,0 +1,14 @@
+package BOJ_JAVA;
+
+import java.util.Scanner;
+
+public class BOJ1000 {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+    }
+}
