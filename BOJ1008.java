33fca6e2d8cef62b8eb6c4fff02b4bc9f5f3ff48
kimhj02 <140468353+kimhj02@users.noreply.github.com>
2024-01-07 19:38:05 +0900

    Add files via upload

---
+++ BOJ1008.java
@@ -0,0 +1,13 @@
+package BOJ_JAVA;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        System.out.println((double)num1 / num2);
+    }
+}
