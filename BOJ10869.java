e3c03376607442b0964e2955e8cbff8dddbd5fbb
kimhj02 <140468353+kimhj02@users.noreply.github.com>
2024-01-07 19:42:13 +0900

    Add files via upload

---
+++ BOJ10869.java
@@ -0,0 +1,17 @@
+package BOJ_JAVA;
+
+import java.util.Scanner;
+
+public class BOJ10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
